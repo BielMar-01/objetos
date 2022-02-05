8679ff27f9569be034c5233811213f57a6d08112
Gabriel <bielmarques922@gmail.com>
2022-02-05 20:08:04 -0300

    Constructor Function - Parte 1 e 2

---
+++ 01-construction_function/script-aula1.js
@@ -0,0 +1,34 @@
+// EXEMPLO REAL
+// Quando mudamos a propriedade seletor, o objeto Dom irá passar a selecionar o novo seletor em seus métodos.
+const Dom = {
+  seletor: 'li',
+  element() {
+    return document.querySelector(this.seletor);
+  },
+  ativo() {
+    this.element().classList.add('ativo');
+  },
+}
+
+Dom.ativo(); // adiciona ativo ao li
+Dom.seletor = 'ul';
+Dom.ativo(); // adiciona ativo ao ul
+
+
+// CONSTRUCTOR FUNCTION REAL
+// Um objeto criado com uma Constructor, não irá influenciar em outro objeto criado com a mesma Constructor.
+function Dom() {
+  this.seletor = 'li';
+  const element = document.querySelector(this.seletor);
+  this.ativo = function() {
+    element.classList.add('ativo');
+  };
+}
+
+const lista = new Dom();
+lista.seletor = 'ul';
+lista.ativo();
+
+const lastLi = new Dom();
+lastLi.seletor = 'li:last-child';
+lastLi.ativo();
