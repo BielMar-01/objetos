9d1d4209495847dba298e918718ae2ad180b7535
Gabriel Marques <bielmarques922@gmail.com>
2022-02-20 18:52:56 -0300

    Array e Iteração - Parte 3

--- 07-array_e_iteracao/script.js
+++ 07-array_e_iteracao/script.js
@@ -255,4 +255,49 @@ const numeros = [6, 43, 22, 88, 101, 29];
 const maiorQue3 = numeros.every(x => x > 3); // true
 
 
+// [].FIND() E [].FINDINDEX()
+// [].find(), retorna o valor atual da primeira iteração que retornar um valor truthy. Já o [].findIndex(), ao invés de retornar o valor, retorna o index deste valor na array.
+const frutas = ['Banana', 'Pêra', 'Uva', 'Maçã'];
+const buscaUva = frutas.findIndex((fruta) => {
+  return fruta === 'Uva'; 
+}); // 2
 
+const numeros = [6, 43, 22, 88, 101, 29];
+const buscaMaior45 = numeros.find(x => x > 45); // 88
+
+
+// [].FILTER()
+// [].filter(), retorna uma array com a lista de valores que durante a sua iteração retornaram um valor truthy.
+const frutas = ['Banana', undefined, null, '', 'Uva', 0, 'Maçã'];
+const arrayLimpa = frutas.filter((fruta) => {
+  return fruta; 
+}); // ['Banana', 'Uva', 'Maçã']
+
+const numeros = [6, 43, 22, 88, 101, 29];
+const buscaMaior45 = numeros.filter(x => x > 45); // [88, 101]
+
+
+// FILTER EM OBJETOS
+const aulas = [
+  {
+    nome: 'HTML 1',
+    min: 15
+  },
+  {
+    nome: 'HTML 2',
+    min: 10
+  },
+  {
+    nome: 'CSS 1',
+    min: 20
+  },
+  {
+    nome: 'JS 1',
+    min: 25
+  },
+]
+
+const aulasMaiores = aulas.filter((aula) => { 
+  return aula.min > 15;
+});
+// [{nome: 'CSS 1', min: 20}, {nome: 'JS 1', min: 25}]
